04aaf39982b691a664daf98351022ddcdd1bbe96
wangyg <wangyg193@chinaunicom.cn>
2020-05-17 15:44:05 +0800

    增加队列结构单元测试

--- datastruct/queue_test.go
+++ datastruct/queue_test.go
@@ -8,4 +8,23 @@ func TestQueuePush(t *testing.T) {
 	queue.Push(10)
 	queue.Push(100)
 	queue.Traverse()
+	t.Logf("the length of queue is:%d\n",queue.GetLen())
+}
+
+func TestQueuePop(t *testing.T) {
+	queue := NewQueue()
+	queue.Push(1)
+	queue.Push(2)
+	queue.Push(3)
+	queue.Push(4)
+	queue.Push(5)
+	queue.Push(6)
+	queue.Traverse()
+	t.Logf("############# Pop 1 ###############\n")
+	queue.Pop()
+	queue.Traverse()
+	t.Logf("############# Pop 2 ###############\n")
+	queue.Pop()
+	queue.Traverse()
+	t.Logf("the length of queue is:%d\n",queue.GetLen())
 }
